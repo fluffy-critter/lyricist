ba22cf679aeb905ff2fd145909d64ca5f5a3e2b1
fluffy <fluffy@beesbuzz.biz>
2025-06-29 02:08:41 -0700

    initial checkin

---
+++ lyricist/__init__.py
@@ -0,0 +1,47 @@
+""" scan songs that are missing lyrics, add the lyrics """
+
+import mutagen
+import os
+import os.path
+import whisper
+import demucs
+import logging
+
+LOGGER=logging.getLogger(__name__)
+
+class Context:
+    def __init__(self, args):
+        self.options = args
+        self.model = whisper.load_model(args.model)
+
+    def scan_file(self, path, root_dir):
+        """ scan a file """
+        name, ext = os.path.splitext(path)
+        if ext != '.mp3':
+            return
+
+        outdir = os.path.join(self.options.output_dir,
+            os.path.dirname(os.path.relpath(path, root_dir)))
+        os.makedirs(outdir, exist_ok=True)
+        LOGGER.debug("scanning %s", path)
+
+        outfile = os.path.join(outdir, os.path.basename(name)) + '.txt'
+        if os.path.isfile(outfile):
+            LOGGER.debug("Lyric file %s already exists", outfile)
+            return
+
+        lyrics = self.model.transcribe(path, verbose=True)
+        lines = '\n'.join([seg.get('text','')
+             for seg in lyrics.get('segments', [])])
+        if lines:
+            LOGGER.debug("Got %s lyrics:\n%s", lyrics.get('language'), lines)
+        with open(outfile, 'w') as output:
+            output.write(lines)
+
+    def scan_dir(self, basedir):
+        """ scan a directory """
+        LOGGER.info("Scanning directory %s", basedir)
+        for root, dirs, files in os.walk(basedir):
+            for name in files:
+                self.scan_file(os.path.join(root, name), basedir)
+
---
+++ lyricist/__main__.py
@@ -0,0 +1,48 @@
+""" entry point """
+
+from . import Context
+import os.path
+import logging
+import argparse
+
+LOG_LEVELS = [logging.WARNING, logging.INFO, logging.DEBUG]
+LOGGER=logging.getLogger(__name__)
+
+def parse_args():
+    """ get the commandline arguments """
+    parser = argparse.ArgumentParser("Figure out songs' lyrics")
+
+    parser.add_argument("-v", "--verbosity", action="count",
+                        help="Increase output logging level", default=0)
+
+    parser.add_argument('inputs', nargs='+', help='Input directories and files')
+    parser.add_argument('--output-dir', '-o', help='Output directory for lyrics',
+        default='.')
+
+    parser.add_argument('--model', '-m', help="Whisper model to use",
+        default='turbo')
+
+    return parser.parse_args()
+
+
+def main():
+    """ entry point """
+    args = parse_args()
+
+    logging.basicConfig(level=LOG_LEVELS[min(
+        args.verbosity, len(LOG_LEVELS) - 1)],
+        format='%(message)s')
+
+    ctx = Context(args)
+
+    for fname in args.inputs:
+        if os.path.isdir(fname):
+            ctx.scan_dir(fname)
+        elif os.path.isfile(fname):
+            ctx.scan_file(fname, os.path.dirname(fname))
+        else:
+            logger.WARNING("Not a file or directory: %s", fname)
+
+if __name__ == '__main__':
+    main()
+
